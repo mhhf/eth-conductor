1fb528534fc05a742b52e44ea34f293ecc88a1ba
Denis <denis.erfurt@gmail.com>
2021-12-29 17:02:07 +0100

    clean flake

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Exposes the eth-conductor module";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-21.11";
@@ -22,35 +22,5 @@
           ];
         };
         nixosModule = import ./eth-conductor.nix;
-      })) // {
-        nixosConfigurations.container = nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules =
-            [ ({ pkgs, ... }: {
-                boot.isContainer = true;
-
-                # Let 'nixos-version --json' know about the Git revision
-                # of this flake.
-                system.configurationRevision = nixpkgs.lib.mkIf (self ? rev) self.rev;
-
-                # Network configuration.
-                networking.useDHCP = false;
-                networking.firewall.allowedTCPPorts = [ 30311 30303 8501 ];
-                networking.firewall.allowedUDPPorts = [ 30311 30303 8501 ];
-
-                users.users = {
-                  mhhf = {
-                    isNormalUser = true;
-                    home = "/home/mhhf";
-                    description = "mhhf";
-                    uid = 1000;
-                  };
-                };
-
-                services.ethConductor.enable = true;
-              })
-              ./eth-conductor.nix
-            ];
-        };
-      };
+      }));
 }
